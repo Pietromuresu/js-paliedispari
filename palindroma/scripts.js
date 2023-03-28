94ab01c347fa24744019f319be2432cb7274eee2
Pietromuresu <muresupietro66@gmail.com>
2023-03-28 23:39:32 +0200

    in progress

---
+++ palindroma/scripts.js
@@ -0,0 +1,27 @@
+
+
+const userWord = prompt(`inserisci una parola per scoprire se è palindroma`);
+
+const arrayWord = userWord.split('')
+
+
+
+
+if (polindromeCheck(arrayWord, reversedWord) ){
+  const message = 'bravo' 
+}else {
+  const message = 'non bravo';
+  console.log(message); 
+};
+  
+for (let i = arrayWord.length - 1; i >= 0; i-- ){
+ const reversedWord = arrayWord[i]; 
+}
+
+function polindromeCheck(word, reverse) {
+
+if (word === reverse) {
+  const word = true;
+  ('bravo')
+  }
+}
